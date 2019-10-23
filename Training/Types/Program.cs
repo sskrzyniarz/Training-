c5ee21a66012db431fe1d1f529227efc1553ac7f
Stephanie Wesley <sskrzyniarz@uwm.com>
2019-10-23 14:58:52 -0400

    Added Strings folder with Base class for start point

--- Training/Types/Program.cs
+++ Training/Types/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-
+﻿
 namespace Types
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Strings.Base.Run();
         }
     }
 }
---
+++ Training/Types/Strings/Base.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Types.Strings
+{
+    public class Base
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
